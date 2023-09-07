fc05e5d39405f6c0f9923096db6d27247588b14f
efa07 <efatariku07@gmail.com>
2023-09-07 11:20:17 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
-  * main - print line followd byb standard error
-  * Return 1
-  */
+ * main - print line followed by standard error.
+ *
+ * Return: Always 1.
+ */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\"
+			- Dora Korpar, 2015-10-19\n", stdout);
 	return (1);
 }
