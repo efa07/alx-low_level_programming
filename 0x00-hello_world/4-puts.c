98ee569cde03ebdd1b6bcbf4086983db86582f48
efa07 <efatariku07@gmail.com>
2023-09-07 09:52:10 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
- *main- is the entery
- *return: will stop main funcation
- */
+*main - entry point funcation
+*return: always 0 to end main
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
+
