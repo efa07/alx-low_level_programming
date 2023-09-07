a8230e503d0e8f842913ce1eaaa3f65ad3b11cec
efa07 <efatariku07@gmail.com>
2023-09-07 09:56:22 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 /**
-*main - entry point funcation
-*return: always 0 to end main
-*/
+ * main - Prints Programming is like building a multilingual puzzle.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
