c144735ecdadb6859e627614750becb680192d57
efa07 <efatariku07@gmail.com>
2023-09-08 13:13:02 -0700

    aplhabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char c;
+
+	char b;
+
+	c = 'a';
+	b = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(b <= 'z') {
+			putchar (b);
+			b++;
+		}
+	putchar('\n');
+	return (0);
+}
