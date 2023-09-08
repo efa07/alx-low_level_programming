096ed492d54aba7d42b59aa26f76617c0e889cf4
efa07 <efatariku07@gmail.com>
2023-09-08 12:49:55 -0700

    print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - The entry point of the program.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char a;
+
+	a = 'b';
+	while
+		(a <= 'z') {
+			putchar(a);
+			a++;
+		}
+	putchar('\n');
+			return (0);
+}
