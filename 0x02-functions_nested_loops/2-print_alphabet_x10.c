a6f5e06b018a7f0b409db1467a0495f4a253a7b5
efa07 <efatariku07@gmail.com>
2023-09-13 08:01:24 -0700

    error fixed

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -12,11 +12,11 @@ void print_alphabet_x10(void)
 
 	while (i < 10)
 	{
+		_putchar('\n');
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
 		}
 		i++;
 	}
-	_putchar('\n');
 }
