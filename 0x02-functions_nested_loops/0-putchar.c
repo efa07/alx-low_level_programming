be3de3b1a12e875b5ffc75c7babe9b8124904611
efa07 <efatariku07@gmail.com>
2023-09-13 06:23:24 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <unistd.h>
+#include <stdio.h>
+/**
+  * main - The entry point of the program.
+  *
+  * Return: Always 0.
+  */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contain C - Functions, nested loops projects
