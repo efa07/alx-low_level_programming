d41d8c05b119b4253f360b50a28f0396b1e4996b
efa07 <efatariku07@gmail.com>
2023-09-14 04:41:43 -0700

    101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - computes and prints the sum of all the multiples
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	unsigned long int sum = 0;
+
+	for (int i = 0; i < 1024; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+		{
+			sum += i;
+		}
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
