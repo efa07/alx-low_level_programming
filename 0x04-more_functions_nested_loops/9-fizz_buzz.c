c4787cb4a456d3c9ea0278e7196d06b6f987a832
Efa Tariku <efatariku07@gmail.com>
2023-09-16 23:25:44 +0300

    9-fizz_buzz.c

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,10 +5,10 @@
  * main - Entry piont
  * Return: Always 0
  */
-int main()
+int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
@@ -29,5 +29,5 @@ int main()
 		}
 	}
 	printf("\n");
-	return 0;
+	return (0);
 }
