a61c908a565df5711625b9e7ada96c8f610878d0
Efa Tariku <efatariku07@gmail.com>
2023-09-16 22:29:57 +0300

    2-main.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *mul - multiplies two ints.
+ *@a: int
+ *@b: int
+ *
+ *Return: multiple of two int
+ */
+int mul(int a, int b)
+{
+	int product;
+
+	product = a * b;
+	return (product);
+}
