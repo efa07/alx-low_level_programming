49496b3491a31509b0846520bda9fa06b0b9cee8
Efa Tariku <efatariku07@gmail.com>
2023-09-21 20:14:45 +0300

    error fixed

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -6,20 +6,23 @@
  * @n: amount of bytes from source.
  * Return: pointer
  */
-char *_strncpy(char *dest, char *src, int n) {
-  int i = 0;
-  while (i < n && src[i] != '\0') {
-    dest[i] = src[i];
-    i++;
-  }
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
 
-  if (i < n) {
-    while (i < n) {
-      dest[i] = '\0';
-      i++;
-    }
-  }
-
-  return dest;
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (i < n)
+	{
+		while (i < n)
+		{
+			dest[i] = '\0';
+			i++;
+		}
+	}
+	return (dest);
 }
 
