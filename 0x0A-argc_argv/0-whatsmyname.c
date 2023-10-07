6b5a06e2406a22da96b0cc6ab3f5132b731e288d
Efa Tariku <efatariku07@gmail.com>
2023-10-07 21:46:49 +0300

    mynameis

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <string.h>
 /**
  * main - print its name, followed by new line.
  * @argc: num of command line arg
@@ -9,6 +9,18 @@
 
 int main(int argc __attribute__((unused)), char *argv[])
 {
+	if (argc > 0)
+	{
+		char *lastocc = strrchr(argv[0], '/');
+		if (lastocc != NULL)
+		{
+			lastocc++;
+		}
+		else
+		{
+			lastocc = argv[0];
+		}
+	}
 	printf("%s", argv[0]);
 	return (0);
 }
