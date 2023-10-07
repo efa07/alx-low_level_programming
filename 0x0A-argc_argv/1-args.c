cf3d2810348e8ae0a4c2a77f2e604153b145d3fd
Efa Tariku <efatariku07@gmail.com>
2023-10-07 21:54:51 +0300

    counter

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints number of arguments.
+ * @argc: argument count.
+ * @argv: argument pointer
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
