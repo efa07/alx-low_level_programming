07bc8ab9675ca7396a3cd0d84407649685c2c3aa
Efa Tariku <efatariku07@gmail.com>
2023-10-12 15:15:16 +0300

    number printer

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,9 +13,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	va_start(list, n);
 
-	for(i = 0; i < n; i++)
+	for (i = 0; i < n; i++)
 	{
-		printf("%d",va_arg(list, int));
+		printf("%d", va_arg(list, int));
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
