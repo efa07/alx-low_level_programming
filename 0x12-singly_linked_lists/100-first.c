0ed949e402ed99da183351e22458b0fafba8d56d
Efa Tariku <efatariku07@gmail.com>
2023-10-19 21:14:11 +0300

    print

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * bmain - function to print string.
+ * Return: non.
+ */
+
+void __attribute__ ((constructor)) bmain()
+{
+	printf("You're beat! and yet, you must allow");
+	printf(",\nI bore my house upon my back!\n");
+}
