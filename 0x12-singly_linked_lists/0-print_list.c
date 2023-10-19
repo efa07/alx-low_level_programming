8d1dcddf7e3983333aebaa754a594e0160aa63d8
Efa Tariku <efatariku07@gmail.com>
2023-10-19 20:09:01 +0300

    updated

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,24 @@
+#include "list.h"
+
+/**
+ * print_list - prints all the elemnts of lits_t.
+ * @h: linked list.
+ * Return: number or node.
+ */
+
+size_t print_list(const list_t *h)
+{
+	size_t c;
+
+	c = 0;
+	while (c != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%d] %s\n", 0, "(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		c++;
+	}
+	return (c);
+}
