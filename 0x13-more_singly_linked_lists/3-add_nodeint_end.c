be5a20b596cb7c4200057ee987d0e9cdc05b223a
Efa Tariku <efatariku07@gmail.com>
2023-10-24 16:39:46 +0300

    add

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end - add a new node at the end
+ * of a linked list
+ * @head: head of a list.
+ * @n: n element.
+ *
+ * Return: address of the new element. NULL if it failed.
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	new_node->n = n;
+
+	new_node->next = NULL;
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
+	{
+		listint_t *current = *head;
+
+		while (current->next != NULL)
+		{
+			current = current->next;
+		}
+		current->next = new_node;
+	}
+	return (new_node);
+}
