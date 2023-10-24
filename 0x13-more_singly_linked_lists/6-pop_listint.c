fd47c03051720bb21fcfabcc158e9b393b87c13c
Efa Tariku <efatariku07@gmail.com>
2023-10-24 17:00:16 +0300

    return node

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,11 +9,11 @@
  */
 int pop_listint(listint_t **head)
 {
-	if (*head == NULL)
-		return (0);
 	int data = (*head)->n;
 	listint_t *temp = *head;
 
+	if (*head == NULL)
+	return (0);
 	*head = (*head)->next;
 	free(temp);
 
