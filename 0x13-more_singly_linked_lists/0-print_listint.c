d842f0069d8f0cd4de3295f3a2739941bd850b44
Efa Tariku <efatariku07@gmail.com>
2023-10-24 16:18:37 +0300

    more nodes

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints elements of a nodes.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t n = 0;
+
+	while (h != NULL)
+	{
+	printf("%d\n", h->n);
+	h = h->next;
+	n++;
+	}
+
+	return (n);
+}
