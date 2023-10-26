d6e38aebefac4792078b413bf3d488a588c295c1
Efa Tariku <efatariku07@gmail.com>
2023-10-26 18:47:43 +0300

    print binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_binary - prints the binary representation of a number.
+ * @n: unsigned long int.
+ * Return: no return.
+ */
+void print_binary(unsigned long int n)
+{
+	unsigned long int mask = 1UL << (sizeof(unsigned long int) * 8 - 1);
+	int leading_zeros = 1;
+	
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+	while (mask)
+	{
+		if (n & mask)
+		{
+			leading_zeros = 0;
+			_putchar('1');
+		}
+		else if (!leading_zeros)
+		{
+			_putchar('0');
+		}
+		mask >>= 1;
+	{
+}
