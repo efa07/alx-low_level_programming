7875de96d9dacf0de6a1c584a84e438d156160bf
efa07 <efatariku07@gmmail.com>
2023-12-07 18:04:57 +0300

    sum

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * sum_dlistint - returns the sum of all nodes
+ * @head: pointer to head.
+ * 
+ * Return: returns the sum
+ */
+int sum_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp = head;
+	int sum = 0;
+
+	if (head == NULL)
+		return (0);
+
+	while (temp != NULL)
+	{
+		sum = sum + temp->n;
+		temp = temp->next;
+	}
+	return (sum);
+}
