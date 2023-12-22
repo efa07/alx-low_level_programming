3ee40147a3219685ca0f17bbcc73292b03d4a713
Efa <efatariku07@gmail.com>
2023-12-22 21:12:26 +0300

    bug fixed

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,19 +9,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash-t;
+	hash_table_t *hash_t;
 	unsigned long int i;
 
-	hash-t = malloc(sizeof(hash_table_t));
-	if (hash-t == NULL)
+	hash_t = malloc(sizeof(hash_table_t));
+	if (hash_t == NULL)
 		return (NULL);
 
-	hash-t->size = size;
-	hash-t->array = malloc(sizeof(hash_node_t *) * size);
-	if (hash-t->array == NULL)
+	hash_t->size = size;
+	hash_t->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_t->array == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
-		hash-t->array[i] = NULL;
+		hash_t->array[i] = NULL;
 
-	return (hash-t);
+	return (hash_t);
 }
